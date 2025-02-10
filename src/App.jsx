47378bffd6c6fbe1aa6d4838fa601e20f5eedb5c
Alexloven <reyvaldiviaalejandro@gmail.com>
2025-02-10 22:29:42 +0100

    prueba de la api

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
 import './App.css'
+import UseFetch from './components/UseFetch'
 
 function App() {
+
+  const { datos, error, loading } = UseFetch('https://adrian.informaticamajada.es/api/asociaciones');
+
+  if (loading) return <p>Loading...</p>
+  if (error) return <p>Error: {error.message}</p>
 
   return (
     <>
       <div className='w-full h-full flex justify-center items-center'>
-        <p>Alejandro y Adri s</p>
+        {datos.map(
+          (item) => (
+            <div key={item.id}>
+              <h2>{item.nombre}</h2>
+            </div>
+          )
+        )}
       </div>
     </>
   )
