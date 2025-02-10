4cc371b0c74a4b3e771bd1f41eb9b61cc2f7b8c7
Alexloven <reyvaldiviaalejandro@gmail.com>
2025-02-10 19:21:34 +0100

    error add

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,6 @@
 import './App.css'
-import { UseFetch } from './components/useFetch'
 
 function App() {
-
-  const { datos, error, loading } = UseFetch('https://adrian.informaticamajada.es/api/asociaciones')
-
-  console.log(datos)
-  if (loading) return <p>Cargando...</p>
-  if (error) return <p>Error</p>
 
   return (
     <>
