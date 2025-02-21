e4f41301656976570b905132199f28efd1292a16
Alexloven <reyvaldiviaalejandro@gmail.com>
2025-02-21 05:16:37 +0100

    update and add: hice la pagina mas responsive y arregle el header y otros models, añadi el modal de perfil y el modal desplegable del gestor

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,51 @@
 import { Outlet } from "react-router";
 import Logo from "../resource/images/logo.png";
 import Navbar from "./Navbar";
+import { Avatar, Badge, Divider, IconButton, Stack } from "@mui/material";
+import MailIcon from '@mui/icons-material/Mail';
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import { Search } from "@mui/icons-material";
+import SearchIcon from '@mui/icons-material/Search';
+import ModalProfile from "./ModalProfile";
+
+const StyledBadge = styled(Badge)(({ theme }) => ({
+    '& .MuiBadge-badge': {
+        right: -3,
+        top: 13,
+        border: `2px solid ${theme.palette.background.paper}`,
+        padding: '0 4px',
+    },
+}));
 
 const Header = () => {
+
+    const [modalProfile, setModalProfile] = React.useState(false);
+
+    const handleClick = () => {
+
+        if (modalProfile) {
+            setModalProfile(false);
+        }
+        else {
+            setModalProfile(true);
+        }
+    }
 
     return (
         <>
             <div className="sticky min-h-min top-0 z-3">
-                <div className="bg-sky-400 text-white text-xs py-1.5 flex justify-center items-center">
+                <div className="bg-sky-600 text-white text-xs py-1 flex justify-center items-center">
                     FREE CLUB OF ASOCIACIONES | CANARY ISLAND
                 </div>
                 <div className="bg-white flex flex-row justify-between items-center px-3 border-b-1 border-gray-300">
                     {/* LOGO */}
-                    <div className="flex flex-row items-center space-x-12">
+                    <div className="flex flex-row items-center space-x-5">
                         <div>
                             <img src={Logo} alt="" width={130} />
                         </div>
-                        <div>
+                        <div className="flex md:hidden">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-8">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
                             </svg>
@@ -25,30 +54,42 @@ const Header = () => {
                     {/* NAVBAR  */}
                     <Navbar />
                     {/* USER Y CARRITO */}
-                    <div className="flex flex-row justify-between items-center space-x-6">
+                    <div className="flex flex-row py-2.5 justify-between items-center space-x-6">
 
-                        <div className="text-black relative">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-5">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
-                            </svg>
+                        <div className=" relative ">
+                            <Search>
+                                <SearchIcon />
+                            </Search>
                         </div>
-                        <div className="text-black relative">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-5">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
-                            </svg>
+                        <div className="relative">
+                            <IconButton aria-label="cart">
+                                <StyledBadge badgeContent={4} color="primary">
+                                    <ShoppingCartIcon />
+                                </StyledBadge>
+                            </IconButton>
                         </div>
-                        <div className="text-black relative ">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-5">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75" />
-                            </svg>
+
+                        <div className="relative">
+                            <Badge badgeContent={4} color="primary">
+                                <MailIcon color="action" />
+                            </Badge>
                         </div>
-                        <div className="w-[40px] h-[40px] relative shrink-0">
-                            <img className="w-full h-full rounded-full object-cover absolute" src="https://plus.unsplash.com/premium_photo-1689977968861-9c91dbb16049?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8Zm90byUyMGRlJTIwcGVyZmlsfGVufDB8fDB8fHww" alt="" />
-                            <div className=" p-0.5 absolute bottom-0 right-0 text-white rounded-full bg-gray-800">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-3">
-                                    <path stroke-linecap="round" stroke-linejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
-                                </svg>
-                            </div>
+
+                        <div>
+                            <Stack className="cursor-pointer" onClick={() => handleClick()} direction="row" spacing={2}>
+                                <Avatar
+                                    alt="Alex"
+                                    src="/static/images/avatar/1.jpg"
+                                    sx={{
+                                        width: 40, height: 40, '&:hover': {
+                                            backgroundColor: 'rgba(0, 0, 100)',
+                                        }
+                                    }}
+                                />
+                            </Stack>
+                            {modalProfile && (
+                                <ModalProfile />
+                            )}
                         </div>
                     </div>
                 </div>
